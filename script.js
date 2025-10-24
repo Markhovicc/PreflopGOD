dd8143f388a87f4431104dfc902b854d0647e933
Markhovicc <marcossbartzz@gmail.com>
2025-10-23 23:22:53 -0300

    mesaebotoes

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("fold").onclick = () => alert("Você deu FOLD 🧤");
+document.getElementById("limp").onclick = () => alert("Você deu LIMP 🟢");
+document.getElementById("raise").onclick = () => alert("Você deu RAISE 2 💰");
+document.getElementById("allin").onclick = () => alert("Você foi ALL-IN 🔥");
